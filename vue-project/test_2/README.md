c1173ee00beb308b8a8a7658c646ac88ff66965f
rimq14 <2309508482@qq.com>
2022-05-07 16:03:47 +0800

    删除多余文件夹

--- vue-project/test_2/README.md
+++
@@ -1,2 +0,0 @@
-# model
-A model project just be used for explain the second method for xiaoa package.
--- vue-project/test_2/src/components/clickMaskA.vue
+++
@@ -1,6 +0,0 @@
-<template>
-<div>
-<h1>我是A组件</h1>
-<router-link :to="{path:'/clickMaskB'}"><h3>跳转到B组件</h3></router-link>
-</div>    
-</template>
